03aa7602e8c92270180b95d06650e9e76a40dadf
answer0424 <wonwon123123@naver.com>
2024-11-21 10:32:48 +0900

    Initial Commit

---
+++ src/main/java/com/lec/spring/domain/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.lec.spring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberDTO {
+}
